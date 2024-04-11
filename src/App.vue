34bd3c97597ca45b172cebefbd5d8ac565fb4607
Евгений Сергеев <sergeevem@rubezh.ru>
2024-04-11 13:39:55 +0400

    feat: init commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import Weather from './components/Weather.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <Weather/>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{}>()
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+</style>
